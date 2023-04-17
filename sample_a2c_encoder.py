69fdae878a5def6a567d077acd489ff0bc88b263
Itay KanHar <knaan.harpaz@gmail.com>
2023-04-17 18:06:04 +0300

    Working sample

--- sample_a2c_encoder.py
+++ sample_a2c_encoder.py
@@ -1,4 +1,6 @@
 from a2c import A2CGPTEncoderConfig, A2CGPTEncoderModel
 
 encoder = A2CGPTEncoderModel(A2CGPTEncoderConfig())
-print(encoder.sample_nograd("Hello world!", 0, 0, 1))
+actions, losses = encoder.sample_nograd("Hello", 0, 0, 1)
+for action in actions:
+    print(action)
